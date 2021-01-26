e972a8665fcd7cca4a6fcddd3e0abf85fc1c3450
Prakhar Jain <theprakharjain@gmail.com>
2021-01-26 20:00:53 +0530

    added comment

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+# Database Model
 class Post(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
